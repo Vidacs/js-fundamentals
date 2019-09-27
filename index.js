8c411cbf69ce83564f0c20a8dfb1c4973110250f
Vidacs <crispim.201132315@gmail.com>
2019-09-27 01:57:49 -0500

    Multiples request

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const API_URL = 'https://swapi.co/api/';
 const PEOPLE_URL = 'people/:id';
 
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id',1)}`;
 const opts = { crossDomain: true};
 
 const onResponse = function(data){
     console.log(`Hola, yo soy ${data.name}`);
 }
 
-$.get(lukeUrl, opts, onResponse);
+function getCharacter(id){
+  const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`;
 
-// $.get(lukeUrl, opts, function(data){
-//     console.log(`Hola, yo soy ${data.name}`);
-// });
+  $.get(url, opts, onResponse);
+}
 
+getCharacter(4);
+getCharacter(5);
+getCharacter(1);
