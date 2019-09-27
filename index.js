5e08037e8d235a7e32557ef19fff36fe6d04690f
Vidacs <crispim.201132315@gmail.com>
2019-09-27 02:04:09 -0500

    Manejo de orden y asincronismo en js

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ const PEOPLE_URL = 'people/:id';
 
 const opts = { crossDomain: true};
 
-const onResponse = function(data){
-    console.log(`Hola, yo soy ${data.name}`);
-}
-
-function getCharacter(id){
+function getCharacter(id, callback){
   const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`;
 
-  $.get(url, opts, onResponse);
+  $.get(url, opts,function(data){
+    console.log(`Hola, yo soy ${data.name}`);
+
+    if(callback){
+      callback();
+    }
+  });
 }
 
-getCharacter(4);
-getCharacter(5);
-getCharacter(1);
+getCharacter(4,function(){
+  getCharacter(5, function(){
+    getCharacter(3, function(){
+      getCharacter(1);    
+    });  
+  });
+});
