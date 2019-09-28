1fffea7f131b3486aa7ce5418ebd166f70a45fbf
Vidacs <crispim.201132315@gmail.com>
2019-09-28 01:05:37 -0500

    Manejo de errores con callbacks

--- index.js
+++ index.js
@@ -6,19 +6,27 @@ const opts = { crossDomain: true};
 function getCharacter(id, callback){
   const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`;
 
-  $.get(url, opts,function(data){
-    console.log(`Hola, yo soy ${data.name}`);
-
-    if(callback){
-      callback();
-    }
+  $.get(url, opts, callback).fail(function(){
+    console.log(`Sucedio un error, no se pudo obtener el parametro  id`);
+    
   });
 }
 
-getCharacter(4,function(){
-  getCharacter(5, function(){
-    getCharacter(3, function(){
-      getCharacter(1);    
+getCharacter(4,function(data){
+  console.log(`Hola, yo soy ${data.name}`);
+
+  getCharacter(5, function(data){
+
+    console.log(`Hola, yo soy ${data.name}`);
+    getCharacter(3, function(data){
+
+      console.log(`Hola, yo soy ${data.name}`);
+
+      getCharacter(1, function(data){
+      
+        console.log(`Hola, yo soy ${data.name}`);
+  
+      });    
     });  
   });
 });
