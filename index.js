a9f88b83cd5c54a5c8e76065d61483f3a8223bce
Vidacs <crispim.201132315@gmail.com>
2019-09-28 01:40:14 -0500

    Promesas en JS

--- index.js
+++ index.js
@@ -4,29 +4,36 @@ const PEOPLE_URL = 'people/:id';
 const opts = { crossDomain: true};
 
 function getCharacter(id, callback){
-  const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`;
-
-  $.get(url, opts, callback).fail(function(){
-    console.log(`Sucedio un error, no se pudo obtener el parametro  id`);
-    
-  });
+  return new Promise((resolve, rejected) => {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`;
+    $
+    .get(url, opts, function(data){
+      resolve(data)
+    })
+    .fail(() => {
+      rejected(id)
+    });
+  })
 }
 
-getCharacter(4,function(data){
-  console.log(`Hola, yo soy ${data.name}`);
-
-  getCharacter(5, function(data){
-
-    console.log(`Hola, yo soy ${data.name}`);
-    getCharacter(3, function(data){
-
-      console.log(`Hola, yo soy ${data.name}`);
-
-      getCharacter(1, function(data){
-      
-        console.log(`Hola, yo soy ${data.name}`);
+function onError(id){
+  console.log(`Sucedio un error al obtener el personaje ${id}`);
   
-      });    
-    });  
-  });
-});
+}
+
+getCharacter(4)
+  .then((data) =>{
+    console.log(`Hola, yo soy ${data.name}`);
+  })
+  .catch(onError)
+
+// Promesas
+// valores que aun no conocemos cuando una funcion sincrona o asincrona
+// se ejecute y se resuelva
+//tienen 3 estados
+//pending
+//fullfilled - promesa resuelta
+//rejected
+// para obtener el valor de la promesa podemos llamar al valor .then
+//podemos ejecutar multiples promesas despues del estado de 
+//fullfilled
