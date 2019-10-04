f62955538ac7a41f10e8512db4e6cb25619c3ac4
Vidacs <crispim.201132315@gmail.com>
2019-10-04 11:50:42 -0500

    Closures

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
-/*El contexto (o alcance) de una función es por lo general, window. 
-Así que en ciertos casos, cuando intentamos referirnos a this en alguna parte del código,
-es posible que tengamos un comportamiento inesperado, porque el contexto quizás no sea el que esperamos.
-Existen al menos tres maneras de cambiar el contexto de una función.
+// /*Entiende los closures de JavaScript
+// Un closure, básicamente, es una función que recuerda el estado de las variables al momento de ser invocada, y conserva este estado a través de reiteradas ejecuciones. 
+// Un aspecto fundamental de los closures es que son funciones que retornan otras funciones. */
 
-Usando el método .bind, enviamos la referencia a la función sin ejecutarla, pasando el contexto como parámetro.
-Usando el método .call, ejecutamos inmediatamente la función con el contexto indicado
-Usando el método .apply, es similar a .call pero los parámetros adicionales se pasan como un arreglo de valores */
-
-const ronald = {
-    nombre: 'Ronald' ,
-    apellido: 'Garcia'
+function crearSaludo(finalDeFrase) {
+    return function (nombre){
+        console.log(`Hola ${nombre}${finalDeFrase}`);
+    }
 }
 
-const pedro = {
-    nombre: 'Pedro' ,
-    apellido: 'Martinez'
-}
-functionsaludar(saludo = 'Hola') {
-    console.log(`${saludo}, mi nombre es ${this.nombre}`)
-}
+const saludoArgentino = crearSaludo('che')
+const saludoMexicano = crearSaludo('Güey')
+const saludoColombiano = crearSaludo('Parce')
 
-
-// // const saludarAronald = saludar.bind(ronald)
-// // const saludarApedro = saludar.bind(pedro)
-
-// setTimeout(saludar.bind(ronald , 'Hola Che'), 1000)
-// saludar.call(ronald, 'hola che') 
-saludar.apply(ronald, ['hola che'])
+saludoArgentino('Ronald');
+saludoMexicano('Ronald');
+saludoColombiano('Ronald');
