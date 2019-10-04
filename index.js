690f916d5fb1d4139da2e0c1ad803d3e11066be8
Vidacs <crispim.201132315@gmail.com>
2019-10-04 11:49:10 -0500

    Cambiando el contexto al llamar a una funcion

--- index.js
+++ index.js
@@ -1,181 +1,29 @@
-const light_blue =  document.getElementById('light_blue');
-const violet =  document.getElementById('violet');
-const orange =  document.getElementById('orange');
-const green =  document.getElementById('green');
-const btnPlay = document.getElementById('btnPlay');
-const lastLevel = 3;
+/*El contexto (o alcance) de una función es por lo general, window. 
+Así que en ciertos casos, cuando intentamos referirnos a this en alguna parte del código,
+es posible que tengamos un comportamiento inesperado, porque el contexto quizás no sea el que esperamos.
+Existen al menos tres maneras de cambiar el contexto de una función.
 
-class Message {
-  constructor(){}
+Usando el método .bind, enviamos la referencia a la función sin ejecutarla, pasando el contexto como parámetro.
+Usando el método .call, ejecutamos inmediatamente la función con el contexto indicado
+Usando el método .apply, es similar a .call pero los parámetros adicionales se pasan como un arreglo de valores */
 
-  async showMessageFirstLevel(){
-    Swal.fire({
-      title: 'Sweet!',
-      text: 'Modal with a custom image.',
-      imageUrl: 'https://unsplash.it/400/200',
-      imageWidth: 400,
-      imageHeight: 200,
-      imageAlt: 'Custom image',
-      animation: false
-    })
-  }
-
-  async showGameOver(level) {
-    return Swal.fire({
-        title: `Game over`,
-        text: `Lo sentimos has perdido en el nivel ${level}`,
-        imageUrl: 'images/game_over.gif',
-        imageWidth: 400,
-        imageHeight: 200,
-        imageAlt: 'Custom image',
-        animation: false,
-        allowOutsideClick: false
-    })
+const ronald = {
+    nombre: 'Ronald' ,
+    apellido: 'Garcia'
 }
 
-async showWinner() {
-    return Swal.fire({
-        title: `Felicidades`,
-        text: `Has ganado el juego`,
-        imageUrl: 'images/win.gif',
-        imageWidth: 400,
-        imageHeight: 200,
-        imageAlt: 'Custom image',
-        animation: false,
-        allowOutsideClick: false
-    })
-  }
+const pedro = {
+    nombre: 'Pedro' ,
+    apellido: 'Martinez'
+}
+functionsaludar(saludo = 'Hola') {
+    console.log(`${saludo}, mi nombre es ${this.nombre}`)
 }
 
-class Game extends Message{
-  constructor(){
-    super();
-    this.colors = [light_blue, violet, orange, green];
-    this.start = this.start.bind(this);
-    this.lightSequence = this.lightSequence.bind(this)
-    this.clickEvent = this.clickEvent.bind(this)
-    this.selectColor = this.selectColor.bind(this);
-    this.level = 1;
-    this.toggleStartGame();
-    this.start();
-  }
 
-  start() {
-    this.sequenceGenerator();
-    setTimeout(() => this.nextLevel());
-  }
+// // const saludarAronald = saludar.bind(ronald)
+// // const saludarApedro = saludar.bind(pedro)
 
-  toggleStartGame(){
-    if(btnPlay.classList.contains('hide')){
-      btnPlay.classList.remove('hide');
-    } else {
-      btnPlay.classList.add('hide');
-    }
-  }
-
-  sequenceGenerator(){
-    this.sequence = new Array(lastLevel).fill(0).map( n => Math.floor(Math.random()*4));    
-  }
-
-  nextLevel(){
-    this.subnivel = 0;
-    if(this.level == 1){
-      this.showMessageFirstLevel()
-        .then(response => {
-          setTimeout(this.lightSequence, 500)
-          this.clickEvent();
-        }
-      )
-    }
-    else{
-      this.lightSequence();
-      this.clickEvent();
-    }
-  }
-  
-
-  blinkColor(color){
-    color.classList.add('light');
-    setTimeout(() =>{
-      color.classList.remove('light');
-    }, 350);
-  }
-
-  lightSequence(){
-    for(var i = 0; i < this.level; i++){
-      const color = this.colors[this.sequence[i]];
-      console.log(color);
-
-      setTimeout( () => {
-        this.blinkColor(color);
-      },1000 * i);
-    }
-  }
-
-  clickEvent(){
-    this.colors[0].addEventListener('click', this.selectColor);
-    this.colors[1].addEventListener('click', this.selectColor);
-    this.colors[2].addEventListener('click', this.selectColor);
-    this.colors[3].addEventListener('click', this.selectColor);
-  }
-
-  selectColor(ev){
-    const colorName = ev.target.dataset.color;
-    const colorNumber = this.transformColorToNumber(colorName);   
-    const color = this.colors[colorNumber];
-
-    this.blinkColor(color);
-
-    if(colorNumber === this.sequence[this.subnivel]){
-      this.subnivel++;
-      if(this.subnivel === this.level){
-        this.level++;
-        this.deleteEventClick();
-        if(this.level === (lastLevel + 1)){
-            //gano
-            this.winGame();
-        } else {
-         setTimeout( () => { this.nextLevel()}, 2000);
-        }
-      }
-    } else {
-      // perdio;
-      this.lostGame();
-    }
-  }
-
-  deleteEventClick(){
-    this.colors[0].removeEventListener('click', this.selectColor);
-    this.colors[1].removeEventListener('click', this.selectColor);
-    this.colors[2].removeEventListener('click', this.selectColor);
-    this.colors[3].removeEventListener('click', this.selectColor);
-  }
-
-  transformColorToNumber(color) {
-    switch (color){
-      case 'light_blue':
-        return 0;
-      case 'violet':
-        return 1
-      case 'orange':
-        return 2;
-      case 'green':
-        return 3
-    }
-  }
-
-  winGame(){
-    this.showWinner().then(btnPlay.classList.remove('hide'))
-  }
-  
-  lostGame(){
-      this.deleteEventClick();
-      this.showGameOver(this.level).then(
-        btnPlay.classList.remove('hide')
-    )
-  }
-}
-
-function startGame() {
-  window.game = new Game();
-}
+// setTimeout(saludar.bind(ronald , 'Hola Che'), 1000)
+// saludar.call(ronald, 'hola che') 
+saludar.apply(ronald, ['hola che'])
